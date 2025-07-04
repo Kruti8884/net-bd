7c1fc7dbb418cd81b50f5c2f0abb408fe691fed5
KRUTI8884 <krutidesai0211@gmail.com>
2025-07-04 13:47:12 +0530

    commit

---
+++ t2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_bd
+{
+    internal class t2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Name= KRUTI DESAI");
+            Console.WriteLine("DOB= 02/11/2004");
+            Console.WriteLine("Address= 4,xyz society,Kalawad Road");
+            Console.WriteLine("City= Rajkot");
+            Console.WriteLine("PinCode= 360001");
+            Console.WriteLine("State= Gujarat");
+            Console.WriteLine("Country= India");
+            Console.WriteLine("Email=abc@yail.com");
+        }
+    }
+}
---
+++ t3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_bd
+{
+    internal class t3
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number;
+
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number} is Even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is Odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
