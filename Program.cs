e7ec3a9f137f0ed863624a5152013e9ab8626b25
DEV <DEV@DESKTOP-HM7GSGV>
2025-07-04 13:02:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_bd
+{
+    internal class sum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number 1:");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number 2:");
+            int b=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum of two numbers is:"+ (a + b));
+        }
+    }
+}
